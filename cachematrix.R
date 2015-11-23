2e2c2250c8df7ca6339dc9ad62e9735b8fe921ad
Oscar Corpeno <oscar.corpeno@gmail.com>
2015-11-22 18:26:43 -0600

    Programming Assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,21 @@
-## This functions calcule the inverse matriz and set it to the cache
+## This functions calcule the inverse matrix and set it to the cache
 
 ## This function sets and gets a Matrix to/from the cache 
 
 makeCacheMatrix <- function(x = matrix()) {
-  
-  i <- NULL
-  set <- function(y) {
-    x <<- y
-    i <<- NULL
-  }
-  get <- function() x
-  setInverse <- function(inverseM) i <<- inverseM
-  getInverse <- function() i
-  list(set = set, get = get,
-       setInverse = setInverse,
-       getInverse = getInverse)
-
+        
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverseM) i <<- inverseM
+        getInverse <- function() i
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
+        
 }
 
 
@@ -23,18 +23,18 @@ makeCacheMatrix <- function(x = matrix()) {
 ## makeCacheMatrix and set the result to the cache
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  i <- x$getInverse()
-  if(!is.null(i)) {
-    message("getting cached data")
-    return(i)
-  }
-  data <- x$get()
-  for (a in seq_len(nrow(data)))
-    for (b in seq_len(ncol(data)))
-      i[a,b] <- data[b,a]
-  
-  x$setInverse(i)
-  i
-  
+        ## Return a matrix that is the inverse of 'x'
+        i <- x$getInverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        for (a in seq_len(nrow(data)))
+                for (b in seq_len(ncol(data)))
+                        i[a,b] <- data[b,a]
+        
+        x$setInverse(i)
+        i
+        
 }
